931427fa0cb84098cab10fb7dba7c67cdf7a4280
Edward Frias <edward.frias@lmd.gob.com>
2023-04-14 09:58:58 -0400

    Controllers created

---
+++ UniversityApiBackend/Controllers/CoursesController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.DataAccess;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly UniversityDBContext _context;
+
+        public CoursesController(UniversityDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Courses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Courses>>> GetCourses()
+        {
+          if (_context.Courses == null)
+          {
+              return NotFound();
+          }
+            return await _context.Courses.ToListAsync();
+        }
+
+        // GET: api/Courses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Courses>> GetCourses(int id)
+        {
+          if (_context.Courses == null)
+          {
+              return NotFound();
+          }
+            var courses = await _context.Courses.FindAsync(id);
+
+            if (courses == null)
+            {
+                return NotFound();
+            }
+
+            return courses;
+        }
+
+        // PUT: api/Courses/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCourses(int id, Courses courses)
+        {
+            if (id != courses.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(courses).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CoursesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Courses
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Courses>> PostCourses(Courses courses)
+        {
+          if (_context.Courses == null)
+          {
+              return Problem("Entity set 'UniversityDBContext.Courses'  is null.");
+          }
+            _context.Courses.Add(courses);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCourses", new { id = courses.Id }, courses);
+        }
+
+        // DELETE: api/Courses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCourses(int id)
+        {
+            if (_context.Courses == null)
+            {
+                return NotFound();
+            }
+            var courses = await _context.Courses.FindAsync(id);
+            if (courses == null)
+            {
+                return NotFound();
+            }
+
+            _context.Courses.Remove(courses);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CoursesExists(int id)
+        {
+            return (_context.Courses?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
